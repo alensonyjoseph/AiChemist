1b00f3ce7cecfa66f2e2c6877773a30a0846e262
thealensj <mailalensj@gmail.com>
2023-07-11 00:37:29 +0530

    model

--- frontend/src/components/GenerativeAIModel.js
+++ frontend/src/components/GenerativeAIModel.js
@@ -1,22 +1,50 @@
-// GenerativeAIModel.js
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function GenerativeAIModel() {
+    const [smile, setSmile] = useState("");
+    const [results, setResults] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState("");
+
+    const handlePredictClick = () => {
+        setIsLoading(true);
+        setError(""); 
+
+        axios.post('http://localhost:5000/generate-smile', {
+            smile: smile
+        })
+        .then(response => {
+            setIsLoading(false);
+            setResults(response.data);
+        })
+        .catch(error => {
+            setIsLoading(false);
+            setError(`Error: ${error}`);
+        });
+    };
+
     return (
         <div className="generative-ai-model">
             <h1>Generative AI Model</h1>
-            <div className="under-construction">
-                <h2>🚧 Under Development 🚧</h2>
-                <p>This feature will be coming soon!</p>
-            </div>
-            <p>
-                Our Generative AI Model is an advanced tool designed to predict chemical properties and suggest novel compounds, thereby accelerating the process of drug discovery.
-                We're currently working hard on this feature and it will be available soon. Stay tuned!
-            </p>
-            <p>
-                The potential applications of this AI model are extensive. It will be able to help researchers and scientists to more effectively and efficiently drive forward their research.
-                The development progress is ongoing, and once launched, users will be able to interact with it to facilitate their research.
-            </p>
+            <input 
+                type="text" 
+                value={smile} 
+                onChange={e => setSmile(e.target.value)} 
+                placeholder="Enter SMILE..." 
+            />
+            <button onClick={handlePredictClick} disabled={isLoading}>
+                {isLoading ? "Predicting..." : "Predict"}
+            </button>
+            {error && <div className="error-message">{error}</div>}
+            {results.map((result, index) => (
+                <div key={index}>
+                    <p><strong>Sequence:</strong> {result.sequence}</p>
+                    <p><strong>Score:</strong> {result.score}</p>
+                    <p><strong>Token:</strong> {result.token}</p>
+                    <p><strong>Token String:</strong> {result.token_str}</p>
+                </div>
+            ))}
         </div>
     );
 }
